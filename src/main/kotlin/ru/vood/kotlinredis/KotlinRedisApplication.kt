beeb87af2991818e6ee402d3e90d3bf69c0694cd
vood <balitskY_igor@list.ru>
2023-03-06 12:32:43 +0300

    init

---
+++ src/main/kotlin/ru/vood/kotlinredis/KotlinRedisApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.kotlinredis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRedisApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinRedisApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vood/kotlinredis/KotlinRedisApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.kotlinredis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRedisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
