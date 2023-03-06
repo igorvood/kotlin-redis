f581fac0cfe633693cae3a5bd714193b4e790d54
vood <balitskY_igor@list.ru>
2023-03-06 17:46:04 +0300

    init

--- src/main/kotlin/ru/vood/kotlinredis/runner/Run.kt
+++ src/main/kotlin/ru/vood/kotlinredis/runner/Run.kt
@@ -1,16 +1,36 @@
 package ru.vood.kotlinredis.runner
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
 import ru.vood.kotlinredis.repo.ISomeRepo
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Service
 class Run(val iSomeRepo: ISomeRepo) : CommandLineRunner{
+
+
+     val logger: Logger = LoggerFactory.getLogger(
+        Run::class.java
+    )
+
     override fun run(vararg args: String?) {
 
 
-        (3 .. 4)
+        val begin = LocalDateTime.now()
+        val cnt = 100000
+        (1 ..cnt)
             .map { iSomeRepo.getSomeData(it.toString())  }
+
+        val end = LocalDateTime.now()
+
+
+        val between = ChronoUnit.MILLIS.between(begin, end)
+
+        logger.info("load $cnt continued  $between seconds")
+
 
     }
 }
